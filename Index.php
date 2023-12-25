9c83eddaf5c69514398136b63caa03841c6e140a
PedroHenriqueMoraisSamsonas <131505706+PedroHenriqueMoraesSamsonas@users.noreply.github.com>
2023-12-25 13:58:22 -0300

    first commit

---
+++ Index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loja de Jogos</title>
+    <link  rel="stylesheet" href="CSS/navbar.css">
+    <link  rel="stylesheet" href="CSS/games.css">
+    <link  rel="stylesheet" href="CSS/alinha.css">
+   
+    <style>
+
+        @import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
+    
+    </style>
+
+</head>
+
+<body>
+
+   <input type="checkbox" id="toggle-menu">
+        <div class="menu">
+            <label for="toggle-menu"><img src="img/align-justify.svg"></label>
+            <ul>
+                <li><div class="cent"><div class="espaca"><img src="img/home.svg"style="height:50px;"></div>Home</div></li>
+                <li><div class="cent"><div class="espaca"><img src="img/user.svg"style="height:50px;"></div>Login</div></li>
+                <li><div class="cent"><div class="espaca"><img src="img/shopping-cart.svg"style="height:50px;"></div>Carrinho</div></li>
+                <li><div class="cent"><div class="espaca"><img src="img/book-open.svg"style="height:50px;"></div>Sobre</div></li>
+            </ul>
+        </div>
+    </input>
+
+
+    <div class="combo-jogos">
+        <div class="game"><img src="img/vava.jpg"><div class="escritas"><p>Valorant</p></div></div> 
+        <div class="game"><img src="img/vava.jpg"><div class="escritas"><p>Minecraft</p></div></div>
+        <div class="game"><img src="img/vava.jpg"><div class="escritas"><p>Uno</p></div></div>
+        <div class="game"><img src="img/vava.jpg"><div class="escritas"><p>Cuphead</p></div></div>
+        <div class="game"><img src="img/vava.jpg"><div class="escritas"><p>Portal</p></div></div>
+    </div>
+    <div class="combo-jogos" style="margin-top:180px;">
+        <div class="game"><img src="img/vava.jpg"><div class="escritas"><p>sss</p></div></div> 
+        <div class="game"><img src="img/vava.jpg"><div class="escritas"><p>Valorant</p></div></div>
+        <div class="game"><img src="img/vava.jpg"><div class="escritas"><p>Valorant</p></div></div>
+        <div class="game"><img src="img/vava.jpg"><div class="escritas"><p>Valorant</p></div></div>
+        <div class="game"><img src="img/vava.jpg"><div class="escritas"><p>Valorant</p></div></div>
+        
+    </div>
+
+
+
+    <div class="imagens">
+
+        <img src="img/segunda opção.png"></img>
+        
+    </div>
+    
+    
+</body>
+</html>
